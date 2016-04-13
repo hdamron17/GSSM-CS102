ffa628261ec70a36c268e89f5ef113fdf9cddc8c
Dennis Perea <DPerea17@.gssm.k12.sc.us>
2016-04-12 23:28:55 -0400

    Added several Checks

--- src/Calculator.java
+++ src/Calculator.java
@@ -27,23 +27,87 @@ public class Calculator {
 	/**
 	 * Checks the equation to see if it's syntax is 
 	 * 		correct
+	 *  
 	 */
-	private void check() {
+	private String check() {
 		
 		//TODO Check possible issues in expression 
 		//and throw various Exceptions
 		//!!!Comment each check!!!
+		
+		
+		//Checking Parentheses Number
+		//Counts total of left and right bracket
+		int leftBracket = 0;
+		int rightBracket = 0;
+		for (int i = 0; i < terms.length; i++)
+		{
+			if (terms[i].equals("("))
+				leftBracket++;
+			
+			if (terms[i].equals(")"))
+				rightBracket++;
+		}
+		
+		if (leftBracket != rightBracket)
+			return "Incorrect Bracket Total";
+		//
+		//
+		
+		
+		//
+		//Checking for correct bracket formatting 
+		String line = terms.toString();
+		Stack <String> brack = new Stack<String>();
+
+		//Make a stack with only the brackets in order
+		for (int i = 0; i < terms.length; i++)
+		{
+		if (terms[i].equals("("))
+			brack.push("(");
+		
+		if (terms[i].equals(")"))
+			brack.pop();
+		}		
+		if (!brack.isEmpty())
+			return "Incorrect Bracket Order";
+		//
+		//
+		
+		//Check to see if the character is legal
+		String listOf = "" + NUMS + OPS;
+		boolean legal = false;
+		
+		    for (int j = 0; j < terms.length; j++)
+		    {
+		    	for (int i = 0; i < listOf.length();i++)
+		    	{
+		    		if (terms[j].equals(listOf.charAt(i)))
+		    				legal = true;
+		    	}
+		    }
+		
+		if (legal == false)
+			return "Illegal Character";
+		
+		return "";
 	}
 	
 	/**
 	 * Solves the given equation using Stacks
 	 * @return Returns the double value of the expression
+	 * @throws Exception 
 	 */
-	public double solve() {
+	public double solve() throws Exception {
 		//With the exception of parentheses, priority = OPS.indexOf(op) / 2;
 		//because of integer division
-		int priority = 0;
-		check();
+		int priority = -1;
+		
+		String errMsg = check();
+		if(!errMsg.equals(""))
+				throw new Exception(errMsg);
+		
+		
 		
 		//TODO Solve the expression from String array
 		return 0.0;
